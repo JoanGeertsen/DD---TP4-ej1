7a883bd2f6564e4718b3cbe95c40798f031ce4da
JoanGeertsen <ttlocuelors@gmail.com>
2024-05-11 14:51:35 -0300

    Commit inicial

--- TPherencia/FPersonas.Designer.cs
+++ TPherencia/FPersonas.Designer.cs
@@ -30,30 +30,27 @@
         {
             components = new System.ComponentModel.Container();
             pPersona = new Panel();
-            chEstudiante = new CheckBox();
+            rbEmpleado = new RadioButton();
+            rbEstudiante = new RadioButton();
+            rbPersona = new RadioButton();
             dtFechaNacimiento = new DateTimePicker();
             mtDni = new MaskedTextBox();
-            tApellido = new TextBox();
             tNombre = new TextBox();
             label4 = new Label();
             label3 = new Label();
-            label2 = new Label();
             label1 = new Label();
             pLista = new Panel();
             lCantidad = new Label();
             lbPersonas = new ListBox();
             cbFiltros = new ComboBox();
-            label5 = new Label();
             pEstudiante = new Panel();
-            label8 = new Label();
-            dtFechaIngreso = new DateTimePicker();
             tCarrera = new TextBox();
             label7 = new Label();
             mtLegajo = new MaskedTextBox();
             label6 = new Label();
-            bBuscar = new Button();
+            bMostrar = new Button();
             bGuardar = new Button();
-            bCancelar = new Button();
+            bCerrar = new Button();
             errorProvider = new ErrorProvider(components);
             pPersona.SuspendLayout();
             pLista.SuspendLayout();
@@ -64,35 +61,57 @@
             // pPersona
             // 
             pPersona.BackColor = Color.PaleTurquoise;
-            pPersona.Controls.Add(chEstudiante);
+            pPersona.Controls.Add(rbEmpleado);
+            pPersona.Controls.Add(rbEstudiante);
+            pPersona.Controls.Add(rbPersona);
             pPersona.Controls.Add(dtFechaNacimiento);
             pPersona.Controls.Add(mtDni);
-            pPersona.Controls.Add(tApellido);
             pPersona.Controls.Add(tNombre);
             pPersona.Controls.Add(label4);
             pPersona.Controls.Add(label3);
-            pPersona.Controls.Add(label2);
             pPersona.Controls.Add(label1);
             pPersona.Location = new Point(12, 12);
             pPersona.Name = "pPersona";
-            pPersona.Size = new Size(307, 147);
+            pPersona.Size = new Size(307, 136);
             pPersona.TabIndex = 0;
             // 
-            // chEstudiante
+            // rbEmpleado
             // 
-            chEstudiante.AutoSize = true;
-            chEstudiante.Location = new Point(92, 121);
-            chEstudiante.Name = "chEstudiante";
-            chEstudiante.Size = new Size(81, 19);
-            chEstudiante.TabIndex = 9;
-            chEstudiante.Text = "Estudiante";
-            chEstudiante.UseVisualStyleBackColor = true;
-            chEstudiante.CheckedChanged += checkBox1_CheckedChanged;
+            rbEmpleado.AutoSize = true;
+            rbEmpleado.Location = new Point(209, 108);
+            rbEmpleado.Name = "rbEmpleado";
+            rbEmpleado.Size = new Size(78, 19);
+            rbEmpleado.TabIndex = 11;
+            rbEmpleado.TabStop = true;
+            rbEmpleado.Text = "Empleado";
+            rbEmpleado.UseVisualStyleBackColor = true;
+            // 
+            // rbEstudiante
+            // 
+            rbEstudiante.AutoSize = true;
+            rbEstudiante.Location = new Point(112, 108);
+            rbEstudiante.Name = "rbEstudiante";
+            rbEstudiante.Size = new Size(80, 19);
+            rbEstudiante.TabIndex = 10;
+            rbEstudiante.TabStop = true;
+            rbEstudiante.Text = "Estudiante";
+            rbEstudiante.UseVisualStyleBackColor = true;
+            // 
+            // rbPersona
+            // 
+            rbPersona.AutoSize = true;
+            rbPersona.Location = new Point(13, 108);
+            rbPersona.Name = "rbPersona";
+            rbPersona.Size = new Size(67, 19);
+            rbPersona.TabIndex = 9;
+            rbPersona.TabStop = true;
+            rbPersona.Text = "Persona";
+            rbPersona.UseVisualStyleBackColor = true;
             // 
             // dtFechaNacimiento
             // 
             dtFechaNacimiento.Format = DateTimePickerFormat.Short;
-            dtFechaNacimiento.Location = new Point(92, 92);
+            dtFechaNacimiento.Location = new Point(92, 68);
             dtFechaNacimiento.Name = "dtFechaNacimiento";
             dtFechaNacimiento.Size = new Size(97, 23);
             dtFechaNacimiento.TabIndex = 8;
@@ -101,25 +120,16 @@
             // 
             // mtDni
             // 
-            mtDni.Location = new Point(92, 63);
+            mtDni.Location = new Point(92, 10);
             mtDni.Mask = "00,000,000";
             mtDni.Name = "mtDni";
             mtDni.Size = new Size(197, 23);
             mtDni.TabIndex = 7;
             mtDni.Leave += mtDni_Leave;
             // 
-            // tApellido
-            // 
-            tApellido.Location = new Point(92, 35);
-            tApellido.Name = "tApellido";
-            tApellido.Size = new Size(197, 23);
-            tApellido.TabIndex = 6;
-            tApellido.KeyPress += tApellido_KeyPress;
-            tApellido.Leave += tApellido_Leave;
-            // 
             // tNombre
             // 
-            tNombre.Location = new Point(92, 6);
+            tNombre.Location = new Point(92, 39);
             tNombre.Name = "tNombre";
             tNombre.Size = new Size(197, 23);
             tNombre.TabIndex = 5;
@@ -129,7 +139,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(13, 100);
+            label4.Location = new Point(13, 76);
             label4.Name = "label4";
             label4.Size = new Size(68, 15);
             label4.TabIndex = 4;
@@ -138,25 +148,16 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(13, 66);
+            label3.Location = new Point(13, 13);
             label3.Name = "label3";
             label3.Size = new Size(73, 15);
             label3.TabIndex = 3;
             label3.Text = "Documento:";
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(13, 37);
-            label2.Name = "label2";
-            label2.Size = new Size(54, 15);
-            label2.TabIndex = 2;
-            label2.Text = "Apellido:";
-            // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(13, 9);
+            label1.Location = new Point(13, 42);
             label1.Name = "label1";
             label1.Size = new Size(54, 15);
             label1.TabIndex = 1;
@@ -168,16 +169,15 @@
             pLista.Controls.Add(lCantidad);
             pLista.Controls.Add(lbPersonas);
             pLista.Controls.Add(cbFiltros);
-            pLista.Controls.Add(label5);
             pLista.Location = new Point(325, 12);
             pLista.Name = "pLista";
-            pLista.Size = new Size(194, 255);
+            pLista.Size = new Size(194, 220);
             pLista.TabIndex = 10;
             // 
             // lCantidad
             // 
             lCantidad.AutoSize = true;
-            lCantidad.Location = new Point(12, 231);
+            lCantidad.Location = new Point(12, 191);
             lCantidad.Name = "lCantidad";
             lCantidad.Size = new Size(58, 15);
             lCantidad.TabIndex = 13;
@@ -189,65 +189,34 @@
             lbPersonas.HorizontalExtent = 450;
             lbPersonas.HorizontalScrollbar = true;
             lbPersonas.ItemHeight = 15;
-            lbPersonas.Location = new Point(12, 53);
+            lbPersonas.Location = new Point(12, 39);
             lbPersonas.Name = "lbPersonas";
-            lbPersonas.Size = new Size(170, 169);
+            lbPersonas.Size = new Size(170, 139);
             lbPersonas.TabIndex = 12;
             // 
             // cbFiltros
             // 
+            cbFiltros.DropDownStyle = ComboBoxStyle.DropDownList;
             cbFiltros.FormattingEnabled = true;
             cbFiltros.Items.AddRange(new object[] { "TODOS", "Estudiantes", "NO Estudiantes" });
-            cbFiltros.Location = new Point(12, 24);
+            cbFiltros.Location = new Point(12, 10);
             cbFiltros.Name = "cbFiltros";
             cbFiltros.Size = new Size(170, 23);
             cbFiltros.TabIndex = 11;
-            cbFiltros.Text = "TODOS";
             cbFiltros.SelectedIndexChanged += cbFiltros_SelectedIndexChanged;
-            cbFiltros.KeyPress += cbFiltros_KeyPress;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(12, 6);
-            label5.Name = "label5";
-            label5.Size = new Size(37, 15);
-            label5.TabIndex = 10;
-            label5.Text = "Filtro:";
             // 
             // pEstudiante
             // 
             pEstudiante.BackColor = Color.PaleGreen;
-            pEstudiante.Controls.Add(label8);
-            pEstudiante.Controls.Add(dtFechaIngreso);
             pEstudiante.Controls.Add(tCarrera);
             pEstudiante.Controls.Add(label7);
             pEstudiante.Controls.Add(mtLegajo);
             pEstudiante.Controls.Add(label6);
-            pEstudiante.Location = new Point(12, 165);
+            pEstudiante.Location = new Point(12, 154);
             pEstudiante.Name = "pEstudiante";
-            pEstudiante.Size = new Size(307, 102);
+            pEstudiante.Size = new Size(307, 78);
             pEstudiante.TabIndex = 13;
             pEstudiante.Visible = false;
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(13, 75);
-            label8.Name = "label8";
-            label8.Size = new Size(83, 15);
-            label8.TabIndex = 12;
-            label8.Text = "Fecha Ingreso:";
-            // 
-            // dtFechaIngreso
-            // 
-            dtFechaIngreso.Format = DateTimePickerFormat.Short;
-            dtFechaIngreso.Location = new Point(98, 70);
-            dtFechaIngreso.Name = "dtFechaIngreso";
-            dtFechaIngreso.Size = new Size(97, 23);
-            dtFechaIngreso.TabIndex = 10;
-            dtFechaIngreso.Value = new DateTime(2020, 1, 1, 0, 0, 0, 0);
-            dtFechaIngreso.Leave += dtFechaIngreso_Leave;
             // 
             // tCarrera
             // 
@@ -285,19 +254,18 @@
             label6.TabIndex = 10;
             label6.Text = "Legajo:";
             // 
-            // bBuscar
+            // bMostrar
             // 
-            bBuscar.Location = new Point(12, 273);
-            bBuscar.Name = "bBuscar";
-            bBuscar.Size = new Size(75, 23);
-            bBuscar.TabIndex = 14;
-            bBuscar.Text = "Buscar";
-            bBuscar.UseVisualStyleBackColor = true;
-            bBuscar.Click += bBuscar_Click;
+            bMostrar.Location = new Point(12, 238);
+            bMostrar.Name = "bMostrar";
+            bMostrar.Size = new Size(75, 23);
+            bMostrar.TabIndex = 14;
+            bMostrar.Text = "Mostrar";
+            bMostrar.UseVisualStyleBackColor = true;
             // 
             // bGuardar
             // 
-            bGuardar.Location = new Point(216, 273);
+            bGuardar.Location = new Point(216, 238);
             bGuardar.Name = "bGuardar";
             bGuardar.Size = new Size(75, 23);
             bGuardar.TabIndex = 15;
@@ -305,15 +273,14 @@
             bGuardar.UseVisualStyleBackColor = true;
             bGuardar.Click += bGuardar_Click;
             // 
-            // bCancelar
+            // bCerrar
             // 
-            bCancelar.Location = new Point(444, 273);
-            bCancelar.Name = "bCancelar";
-            bCancelar.Size = new Size(75, 23);
-            bCancelar.TabIndex = 16;
-            bCancelar.Text = "Cancelar";
-            bCancelar.UseVisualStyleBackColor = true;
-            bCancelar.Click += bCancelar_Click;
+            bCerrar.Location = new Point(444, 238);
+            bCerrar.Name = "bCerrar";
+            bCerrar.Size = new Size(75, 23);
+            bCerrar.TabIndex = 16;
+            bCerrar.Text = "Cerrar";
+            bCerrar.UseVisualStyleBackColor = true;
             // 
             // errorProvider
             // 
@@ -323,10 +290,10 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(534, 304);
-            Controls.Add(bCancelar);
+            ClientSize = new Size(534, 271);
+            Controls.Add(bCerrar);
             Controls.Add(bGuardar);
-            Controls.Add(bBuscar);
+            Controls.Add(bMostrar);
             Controls.Add(pEstudiante);
             Controls.Add(pLista);
             Controls.Add(pPersona);
@@ -348,27 +315,24 @@
         private TextBox tNombre;
         private Label label4;
         private Label label3;
-        private Label label2;
         private Label label1;
         private MaskedTextBox mtDni;
-        private TextBox tApellido;
         private DateTimePicker dtFechaNacimiento;
-        private CheckBox chEstudiante;
         private Panel pLista;
         private ListBox lbPersonas;
         private ComboBox cbFiltros;
-        private Label label5;
         private Panel pEstudiante;
         private Label label6;
         private Label lCantidad;
-        private Label label8;
-        private DateTimePicker dtFechaIngreso;
         private TextBox tCarrera;
         private Label label7;
         private MaskedTextBox mtLegajo;
-        private Button bBuscar;
+        private Button bMostrar;
         private Button bGuardar;
-        private Button bCancelar;
+        private Button bCerrar;
         private ErrorProvider errorProvider;
+        private RadioButton rbEmpleado;
+        private RadioButton rbEstudiante;
+        private RadioButton rbPersona;
     }
 }
--- TPherencia/FPersonas.cs
+++ TPherencia/FPersonas.cs
@@ -6,205 +6,56 @@ namespace TPherencia
     public partial class FPersonas : Form
     {
         #region Atributos
-        private Persona[] aPersonas;
-        private Estudiante[] aEstudiantes;
-        private int max;
+        private List<Persona> listPersonas;
         private int cantPersonas;
-        private int cantEstudiantes;
         #endregion
 
         public FPersonas()
         {
             InitializeComponent();
-            max = 50;
-            aPersonas = new Persona[max];
+            listPersonas = new List<Persona>();
             cantPersonas = 0;
-            aEstudiantes = new Estudiante[max];
-            cantEstudiantes = 0;
         }
 
         #region Funcionalidades
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            if (chEstudiante.Checked)
-                pEstudiante.Visible = true;
-            else pEstudiante.Visible = false;
+
         }
 
         private bool existePersona(Persona p)
         {
-            int i = 0;
-            while (i < cantPersonas && !aPersonas[i].esIgual(p))
-                i++;
-            return i < cantPersonas;
+            return true;
         }
 
         private bool existeEstudiante(Estudiante e)
         {
-            int i = 0;
-            while (i < cantEstudiantes && !aEstudiantes[i].esIgual(e))
-                i++;
-            return i < cantEstudiantes;
+            return true;
         }
 
         private bool deseaActualizar(Persona p)
         {
             return MessageBox.Show($"Se encontró:\n\n{p.mostrar()}\n\n¿Desea actualizarlo?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes;
         }
-        private void actualizarOcrearPersona()
-        {
-            Persona p = new Persona(mtDni.Text);
-            if (existePersona(p)) //Actualizo la persona
-            {
-                int i = 0;
-                while (i < cantPersonas && !aPersonas[i].esIgual(p)) i++;
-                if (deseaActualizar(aPersonas[i]))
-                {
-                    aPersonas[i].Nombre = tNombre.Text; aPersonas[i].Apellido = tApellido.Text;
-                    aPersonas[i].FechaNacimiento = dtFechaNacimiento.Text;
-                    MessageBox.Show(aPersonas[i].mostrar(), "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                limpiarCampos();
-            }
-            else//Creo persona 
-            {
-                aPersonas[cantPersonas++] = new Persona(mtDni.Text, tNombre.Text, tApellido.Text, dtFechaNacimiento.Text);
-                MessageBox.Show(aPersonas[cantPersonas - 1].mostrar(), "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
-
-        private void actualizarOcrearEstudiante()
-        {
-            Estudiante e = new Estudiante(mtDni.Text, mtLegajo.Text);
-            if (existeEstudiante(e)) //Actualizo el estudiante
-            {
-                int i = 0;
-                while (i < cantEstudiantes && !aEstudiantes[i].esIgual(e)) i++;
-                if (deseaActualizar(aEstudiantes[i]))
-                {
-                    aEstudiantes[i].Nombre = tNombre.Text; aEstudiantes[i].Apellido = tApellido.Text;
-                    aEstudiantes[i].FechaNacimiento = dtFechaNacimiento.Text;
-                    aEstudiantes[i].Legajo = mtLegajo.Text; aEstudiantes[i].Carrera = tCarrera.Text;
-                    aEstudiantes[i].FechaNacimiento = dtFechaIngreso.Text;
-                    MessageBox.Show(aEstudiantes[i].mostrar(), "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                limpiarCampos();
-            }
-            else//Creo Estudiante 
-            {
-                aEstudiantes[cantEstudiantes++] = new Estudiante(mtDni.Text, tNombre.Text, tApellido.Text, dtFechaNacimiento.Text, mtLegajo.Text, tCarrera.Text, dtFechaIngreso.Text);
-                MessageBox.Show(aEstudiantes[cantEstudiantes - 1].mostrar(), "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-
-
-        }
-
-        private void actualizarListBoxConArreglo(Persona[] a, int tope)
-        {
-            int i = 0;
-            while (i < tope)
-                lbPersonas.Items.Add(a[i++].mostrar());
-        }
 
         private void actualizarListBox()
         {
-            lbPersonas.Items.Clear();
-            if (cbFiltros.SelectedIndex == -1 || cbFiltros.SelectedIndex == 0) //Todos
-            {
-                actualizarListBoxConArreglo(aEstudiantes, cantEstudiantes);
-                actualizarListBoxConArreglo(aPersonas, cantPersonas);
-                lCantidad.Text = $"Cantidad: {cantPersonas + cantEstudiantes}";
-            }
-            else if (cbFiltros.SelectedIndex == 1) //Estudiantes
-            {
-                actualizarListBoxConArreglo(aEstudiantes, cantEstudiantes);
-                lCantidad.Text = $"Cantidad: {cantEstudiantes}";
-            }
-            else //Personas
-            {
-                actualizarListBoxConArreglo(aPersonas, cantPersonas);
-                lCantidad.Text = $"Cantidad: {cantPersonas}";
-            }
+
         }
-        private void limpiarCampos()
-        {
-            tNombre.Clear();tApellido.Clear();mtDni.Clear(); dtFechaNacimiento.Text = "1/1/2000";
-            mtLegajo.Clear(); tCarrera.Clear(); dtFechaIngreso.Text = "1/1/2020";
-        }
+
         private void bGuardar_Click(object sender, EventArgs e)
         {
-            if (!mtDni.MaskCompleted)
-            {
-                MessageBox.Show("Debe ingresar un documento VALIDO", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                errorProvider.SetError(mtDni, "Documento invalido"); mtDni.Focus();
-            }
 
-            else if (chEstudiante.Checked && !mtLegajo.MaskCompleted)
-            {
-                MessageBox.Show("Debe ingresar un legajo VALIDO", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                errorProvider.SetError(mtLegajo, "Legajo invalido"); mtLegajo.Focus();
-            }
-            else if (!chEstudiante.Checked) actualizarOcrearPersona();
-            else actualizarOcrearEstudiante();
-            
-            actualizarListBox();
         }
 
-        private void bCancelar_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
         private void cbFiltros_SelectedIndexChanged(object sender, EventArgs e)
         {
             actualizarListBox();
-        }
-
-        private void bBuscar_Click(object sender, EventArgs e)
-        {
-            if (!mtDni.MaskCompleted)
-            {
-                errorProvider.Clear();
-                MessageBox.Show("Debe ingresar un documento VALIDO", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                errorProvider.SetError(mtDni, "Documento invalido"); mtDni.Focus();
-            }
-
-            else if (chEstudiante.Checked && !mtLegajo.MaskCompleted)
-            {
-                errorProvider.Clear();
-                MessageBox.Show("Debe ingresar un legajo VALIDO", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                errorProvider.SetError(mtLegajo, "Legajo invalido"); mtLegajo.Focus();
-            }
-            else if (!chEstudiante.Checked)//Busco una persona
-            {
-                Persona p = new Persona(mtDni.Text);
-                int i = 0;
-                while(i < cantPersonas && !aPersonas[i].esIgual(p))                
-                    i++;
-                if (i < cantPersonas && cantPersonas > 0) MessageBox.Show($"Se encontró:\n\n{aPersonas[i].mostrar()}","Búsqueda", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                else MessageBox.Show($"No se encontraron resultados", "Búsqueda", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else if (chEstudiante.Checked) //Busco un estudiante
-            {
-                Estudiante aux = new Estudiante(mtDni.Text, mtLegajo.Text); //Revisar donde esta la e global
-                int i = 0;
-                while (i < cantEstudiantes && !aEstudiantes[i].esIgual(aux))
-                    i++;
-                if (i < cantEstudiantes && cantEstudiantes > 0) MessageBox.Show($"Se encontró:\n\n{aEstudiantes[i].mostrar()}", "Búsqueda", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                else MessageBox.Show($"No se encontraron resultados", "Búsqueda", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
         }
         #endregion
 
         #region KeyPress
         private void tNombre_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (!char.IsLetter(e.KeyChar) && e.KeyChar != ' ' && e.KeyChar != (char)Keys.Back)
-            {
-                e.Handled = true;
-            }
-        }
-
-        private void tApellido_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsLetter(e.KeyChar) && e.KeyChar != ' ' && e.KeyChar != (char)Keys.Back)
             {
@@ -219,11 +70,6 @@ namespace TPherencia
                 e.Handled = true;
             }
         }
-
-        private void cbFiltros_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            e.Handled = true;
-        }
         #endregion
 
         #region Validación de campos
@@ -232,13 +78,6 @@ namespace TPherencia
             if (tNombre.Text.Length <= 0)
                 errorProvider.SetError(tNombre, "Nombre invalido");
             else errorProvider.SetError(tNombre, "");
-        }
-
-        private void tApellido_Leave(object sender, EventArgs e)
-        {
-            if (tApellido.Text.Length <= 0)
-                errorProvider.SetError(tApellido, "Apellido invalido");
-            else errorProvider.SetError(tApellido, "");
         }
 
         private void mtDni_Leave(object sender, EventArgs e)
@@ -272,20 +111,7 @@ namespace TPherencia
                 errorProvider.SetError(tCarrera, "Carrera invalida");
             else errorProvider.SetError(tCarrera, "");
         }
-
-        private void dtFechaIngreso_Leave(object sender, EventArgs e)
-        {
-            //Por el momento solo chequeamos que no sea una fecha futura
-            DateTime fechaDeIngreso = dtFechaIngreso.Value;
-            DateTime fechaHoy = DateTime.Today;
-
-
-            if (fechaDeIngreso >= fechaHoy)
-                errorProvider.SetError(dtFechaIngreso, "Fecha invalida");
-            else
-                errorProvider.SetError(dtFechaIngreso, "");
-        }
         #endregion
-       
+
     }
 }
